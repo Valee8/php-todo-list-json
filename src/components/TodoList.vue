07a986d73c531957e0d6101bb7d50c415d1e8a4c
Vale <oirelav95@gmail.com>
2023-01-12 19:01:20 +0100

    creo componente TodoList

---
+++ src/components/TodoList.vue
@@ -0,0 +1,70 @@
+<script>
+
+import axios from 'axios';
+
+const urlApi = "http://localhost/";
+
+export default {
+    name: "TodoList",
+    data() {
+        return {
+            todoList: [],
+            newElem: ''
+        }
+    },
+    methods: {
+        formSubmit(e) {
+            e.preventDefault();
+
+            const params = {
+                params: {
+                    'newElem': this.newElem
+                }
+            };
+
+            axios.get(urlApi + "api-create-todo.php", params)
+
+                .then(() => {
+
+                    this.getElements();
+                });
+        },
+        getElements() {
+            axios.get(urlApi + "api.php")
+                .then(res => {
+
+                    this.todoList = res.data;
+
+                    console.log(this.todoList);
+
+                });
+        }
+    },
+    mounted() {
+        this.getElements();
+    }
+}
+</script>
+
+<template>
+    <h1>
+        Todo List
+    </h1>
+
+    <ul>
+        <li v-for="(list, index) in todoList" :key="index">
+            {{ list.subject }}
+        </li>
+    </ul>
+
+    <form @submit="formSubmit">
+        <input type="text" name="newElem" placeholder="Inserisci elemento..." v-model="newElem">
+        <input type="submit" value="Inserisci">
+    </form>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color: red;
+}
+</style>
