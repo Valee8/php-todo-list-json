6c7489f1ec3ee7e7738e2b427c519615457917fa
Vale <oirelav95@gmail.com>
2023-01-12 19:00:28 +0100

    creo file api-create-todo.php

---
+++ backend/api-create-todo.php
@@ -0,0 +1,22 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+header('Content-Type: application/json');
+
+$newElem = $_GET['newElem'];
+
+$jsonTodoList = file_get_contents("todo.json", true);
+
+$todoList = json_decode($jsonTodoList);
+
+$todoList[] = [
+    "subject" => $newElem,
+    "completed" => false
+];
+
+$jsonTodoList = json_encode($todoList);
+
+file_put_contents("todo.json", $jsonTodoList);
