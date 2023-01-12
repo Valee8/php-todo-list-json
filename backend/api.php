8df8e846f2575ab2aecf20463e3baa0315dd2b6c
Vale <oirelav95@gmail.com>
2023-01-12 19:00:36 +0100

    creo file api.php

---
+++ backend/api.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+header('Content-Type: application/json');
+
+$jsonTodoList = file_get_contents("todo.json", true);
+
+$todoList = json_decode($jsonTodoList);
+
+echo json_encode($todoList);
